52613bb4e6875e0114f9ea3827a1b4c9022b85aa
nack1400 <nack1400@naver.com>
2022-01-01 21:21:53 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict'
+
+//alert("hi");
+
+// const tmp = document.getElementById("title");
+
+// //console.log(tmp);
+
+// tmp.innerText = "변경한 제목";
+
+// const tmp2 = document.getElementsByClassName("hello");
+// console.log(tmp2);
+
+
+// const tmp3 = document.querySelector(".hello h1");
+// console.log(tmp3);
+
+// const tmp4 = document.querySelectorAll(".hello h1");
+// console.log(tmp4);
+
+// const h1 = document.querySelector("div.hello h1");
+// function click(){
+//   console.log("클릭했습니다.");
+// }
+// function mouseEnter(){
+//   h1.innerText = "마우스 올라옴";
+// }
+// function mouseLeave(){
+//   h1.innerText = "마우스 나감";
+// }
+// function windowResize(){
+//   document.body.style.backgroundColor = "tomato";
+// }
+// function windowCopy(){
+//   alert("복사 실행!");
+// }
+// function windowOffline(){
+//   alert("와이파이가 없습니다");
+// }
+// function windowOnline(){
+//   alert("와이파이가 연결되었습니다");
+// }
+
+// h1.addEventListener("click", click);
+// h1.addEventListener("mouseenter", mouseEnter);
+// h1.addEventListener("mouseleave",mouseLeave);
+
+// window.addEventListener("resize", windowResize);
+// window.addEventListener("copy", windowCopy);
+// window.addEventListener("offline",windowOffline);
+// window.addEventListener("online",windowOnline);

ec07d52fe1cbf3c780401fb69d8b993c18017f94
nack1400 <nack1400@naver.com>
2022-01-02 17:31:24 +0900

    form practice

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event){
+  event.preventDefault(); //브라우저의 기본동작을 막는, 에를 들어) 새로고침을 막는 api
+  console.log(event);
+}
+
+function handleLinkClick(event){
+  event.preventDefault();
+  console.dir(event);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);

7f985e30fccf759c1cf7c5d815fe0aaf0319112d
nack1400 <nack1400@naver.com>
2022-01-04 22:46:33 +0900

    update images

---
+++ js/background.js
@@ -0,0 +1,12 @@
+const images = ["1.jpg", "2.png", "3.jpg", "4.png"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// console.log(chosenImage);
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+console.log(bgImage);
+
+document.body.appendChild(bgImage);

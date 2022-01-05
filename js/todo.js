ddbc85cd2ef399b98b3dae235d476d74b5f922f4
nack1400 <nack1400@naver.com>
2022-01-05 21:57:22 +0900

    update todolist

--- js/todo.js
+++ js/todo.js
@@ -15,14 +15,17 @@ function saveToDos(){
 function deleteToDo(event){
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo){
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
 
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   button.innerText ="X";
   button.addEventListener("click", deleteToDo);
 
@@ -35,16 +38,16 @@ function handleToDoSubmit(event){
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value ="";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
-
-function sayHello(item){
-  console.log("this is the turn of", item);
-}
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
@@ -52,4 +55,8 @@ if(savedToDos !== null){
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
+}
+
+function sexyFilter(){
+
 }
